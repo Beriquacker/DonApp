935773c0f4e62a6e48024784e1cd93bdd50d41b2
Gonzalo <gpandelet@gmail.com>
2023-03-22 20:28:21 +0100

    Creacion de la pantalla de bienvenida y el menú.

---
+++ app/src/main/java/com/example/donapp/donapp.java
@@ -0,0 +1,16 @@
+package com.example.donapp;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class donapp extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bienvenida);
+    }
+
+
+}
