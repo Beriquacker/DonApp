a4b8742c9c3ddb52dd63e0592fe0328e482992bf
Gonzalo <gpandelet@gmail.com>
2023-03-24 11:07:46 +0100

    Creacion de la clase registro y puesta en funcionamiento.

---
+++ app/src/main/java/com/example/donapp/Registro.java
@@ -0,0 +1,50 @@
+package com.example.donapp;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageButton;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.util.Log;
+
+
+public class Registro extends AppCompatActivity {
+    private EditText editTextNombre;
+    private EditText editTextEmail;
+    private EditText editTextContraseña;
+    private EditText editTextRepContraseña;
+    private EditText editTextCodPostal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+
+        editTextNombre = findViewById(R.id.editTextTextPersonName);
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress2);
+        editTextContraseña = findViewById(R.id.editTextTextPassword2);
+        editTextRepContraseña = findViewById(R.id.editTextTextPassword3);
+        editTextCodPostal = findViewById(R.id.editTextNumber);
+
+        Button buttonMostrarNombre = findViewById(R.id.button2);
+        buttonMostrarNombre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = editTextNombre.getText().toString();
+                String Contra = editTextContraseña.getText().toString();
+                String ContraRep = editTextRepContraseña.getText().toString();
+                String Email = editTextEmail.getText().toString();
+                String CodPostal = editTextCodPostal.getText().toString();
+
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/donapp/donapp.java
+++ app/src/main/java/com/example/donapp/donapp.java
@@ -1,16 +1,39 @@
 package com.example.donapp;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageButton;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.donapp.Registro;
+
+import java.io.IOException;
 
 public class donapp extends AppCompatActivity {
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bienvenida);
+        setContentView(R.layout.login);
+
+        Button button = findViewById(R.id.registro);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondActivity();
+            }
+        });
     }
 
-
+    private void openSecondActivity() {
+        Intent intent = new Intent(this, Registro.class);
+        startActivity(intent);
+    }
 }
