0439c335718e354b4373dd3b3895af97e02cbfa4
dariomartinx <101869065+dariomartinx@users.noreply.github.com>
2023-05-02 15:41:35 +0200

    add usuario class

---
+++ app/src/main/java/com/example/donapp/Clases/Usuario.java
@@ -0,0 +1,43 @@
+package com.example.donapp.Clases;
+
+public class Usuario {
+    private Integer Id;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String Email;
+
+    public String getCodPostal() {
+        return CodPostal;
+    }
+
+    public void setCodPostal(String codPostal) {
+        CodPostal = codPostal;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    private String CodPostal;
+}
